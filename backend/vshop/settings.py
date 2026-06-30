ed2e9fa1148d048f15b85442cabd31ce955fba3b
SergeyKolesenkov <kolesenkovs@bk.ru>
2026-06-30 05:02:05 +0300

    добавлен kolesenkov.ru в ALLOWED_HOSTS

--- backend/vshop/settings.py
+++ backend/vshop/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-local-developm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kolesenkov.ru',
+    'www.kolesenkov.ru',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
